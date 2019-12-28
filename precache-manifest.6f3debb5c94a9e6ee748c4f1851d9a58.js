e5f3336bd694265ceba9034201bac7b4bbe38b8e
Irina Kramer <irakramer@gmail.com>
2019-12-28 01:51:29 -0800

    Updates

---
+++ precache-manifest.6f3debb5c94a9e6ee748c4f1851d9a58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b364a60aab20752aa969509cb5d78e39",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12d3a9b0e45813802081",
+    "url": "/monsters-rolodex/static/css/main.ab57b779.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12d3a9b0e45813802081",
+    "url": "/monsters-rolodex/static/js/main.90eee9be.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
